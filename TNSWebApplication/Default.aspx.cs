9efc0fd9465f37b503532d8ceabcd20d4827d4a6
snootiecute <dangtiendat288@gmail.com>
2024-01-28 02:19:25 +0700

    add comments

--- TNSWebApplication/Default.aspx.cs
+++ TNSWebApplication/Default.aspx.cs
@@ -17,22 +17,32 @@ namespace TNSWebApplication
             sortService = new SortService.Service1Client();
         }
 
+        // Converts Celsius to Fahrenheit using a temperature conversion service.
+        // Parses the input from the Celsius to Fahrenheit input text box.
+        // Displays the result in the Celsius to Fahrenheit result label.
         protected void c2fButton_Click(object sender, EventArgs e)
-        {             
+        {
             int f = tcService.c2f(int.Parse(c2fInput.Text));
             c2fResLabel.Text = f.ToString();
         }
 
+        // Converts Fahrenheit to Celsius using a temperature conversion service.
+        // Parses the input from the Fahrenheit to Celsius input text box.
+        // Displays the result in the Fahrenheit to Celsius result label.
         protected void f2cButton_Click(object sender, EventArgs e)
-        {            
+        {
             int c = tcService.f2c(int.Parse(f2cInput.Text));
             f2cResLabel.Text = c.ToString();
         }
 
+        // Sorts a string using the sorting service.
+        // Takes the input from the string input text box.
+        // Displays the sorted result in the sorting result label.
         protected void sortButton_Click(object sender, EventArgs e)
         {
             string sortedString = sortService.sort(stringInput.Text);
             sortResLabel.Text = sortedString;
         }
+
     }
 }
